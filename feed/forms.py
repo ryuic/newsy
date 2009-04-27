94fdfe963c3b26e85a79524d3eae20033c658e1f
Youthhr <youthhr@gmail.com>
2009-04-27 23:54:28 +0900

    Feed Form changed.

--- feed/forms.py
+++ feed/forms.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django import forms
-from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _, ugettext as __
 from feed.models import *
 from ragendja.forms import FormWithSets, FormSetField
@@ -12,12 +11,21 @@ class FeedForm(forms.ModelForm):
     url = forms.URLField(widget=forms.TextInput(attrs=dict(maxlength=75)),
          label=_(u'Feed URL'))
 
+    HOURS = [(i+1,i+1) for i in range(24)]
+    execute_hour = forms.MultipleChoiceField(label=_(u'Hours'), 
+        widget=forms.CheckboxSelectMultiple, choices=HOURS)
+
+    MINUTES = [(i+1,i+1) for i in range(60)]
+    execute_minute = forms.MultipleChoiceField(label=_(u'Minutes'), 
+        widget=forms.CheckboxSelectMultiple,choices=MINUTES)
+
     def clean_execute_hour(self):
-        execute_hour = self.cleaned_data['execute_hour'].split(',');
+        logging.info("EXECUTE_HOUR >>> % s", self.cleaned_data['execute_hour'])
+        execute_hour = self.cleaned_data['execute_hour']
         return [int(eh.strip()) for eh in execute_hour]
 
     def clean_execute_minute(self):
-        execute_minute = self.cleaned_data['execute_minute'].split(',');
+        execute_minute = self.cleaned_data['execute_minute']
         return [int(em.strip()) for em in execute_minute]
 
     class Meta:
